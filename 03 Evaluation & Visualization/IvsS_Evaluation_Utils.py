26718a4fabf69311989579e4efff9d7b98bf5ffa
Lanza-git <79972602+Lanza-git@users.noreply.github.com>
2024-07-29 14:37:19 +0200

    update

--- 03 Evaluation & Visualization/IvsS_Evaluation_Utils.py
+++ 03 Evaluation & Visualization/IvsS_Evaluation_Utils.py
@@ -4,7 +4,22 @@ import os
 import pickle
 from scipy.stats import chi2
 
+
+################################################################### Data Handling ###################################################################
+
 def load_metadata(directory):
+    """ Load the metadata for the newsvendor problem from specified location
+    
+    Parameters
+    ---------
+    directory : str
+        Path to the directory containing the metadata files
+    
+    Returns
+    ---------
+    metadata : list
+        List of dictionaries containing the metadata for each run
+    """
     metadata = []
     for file_name in os.listdir(directory):
         if file_name.endswith('meta.pkl'):
@@ -25,11 +40,15 @@ def load_generated_data(path:str, multi:bool=True):
 
     Parameters
     ---------
-    path : path to the data file
+    path : str
+        Path to the file containing the generated data        
+    multi : bool
+        Flag to indicate if the data is multi-item or single-item
     
     Returns
     ---------
     raw_data : pd.dataframe
+        Dataframe containing the loaded data
     """ 
     # Load Data
     with h5py.File(path, 'r') as file:
@@ -53,6 +72,55 @@ def load_cost_structure(alpha_input:np.array, underage_input:np.array, overage_i
     alpha = alpha_input
     underage = underage_input
     overage = overage_input
+
+
+################################################################### Newsvendor related ###################################################################
+
+def get_constants(risk_factor):
+    """ Get the constants for the newsvendor problem
+
+    Parameters
+    ---------
+    risk_factor : float
+        Risk factor for the newsvendor problem
+    
+    Returns
+    ---------
+    underage_data : np.array
+        Multi-item underage cost data
+    overage_data : np.array
+        Multi-item overage cost data
+    alpha_data : np.array
+        Substituion matrix
+    underage_data_single : float
+        Single-item underage cost
+    overage_data_single : float
+        Single-item overage cost
+    """
+    # Parameters for multi-item newsvendor problem
+    prices = np.array([0.3, 0.5, 0.6, 0.5, 0.5, 0.5]) #price data
+    prices = prices.reshape(6,1)
+    costs = np.array([0.06, 0.06, 0.06, 0.06, 0.06, 0.06]) #cost data
+    costs = costs.reshape(6,1)
+    costs = costs * risk_factor
+    salvages = np.array([0.01, 0.01, 0.01, 0.01, 0.01, 0.01]) #salvage data
+    salvages = salvages.reshape(6,1)
+    underage_data = prices - costs 
+    overage_data = costs - salvages 
+    underage_data_single = underage_data[0,0]
+    overage_data_single = overage_data[0,0]
+
+
+
+    alpha_data = np.array([             #alpha data
+        [0.0, 0.11, 0.15, 0.14, 0.14, 0.15],
+        [0.22, 0.0, 0.08, 0.11, 0.12, 0.12],
+        [0.24, 0.07, 0.0, 0.07, 0.08, 0.07],
+        [0.26, 0.09, 0.07, 0.0, 0.12, 0.12],
+        [0.19, 0.10, 0.10, 0.12, 0.0, 0.14],
+        [0.17, 0.13, 0.11, 0.11, 0.13, 0.0]
+    ])
+    return underage_data, overage_data, alpha_data, underage_data_single, overage_data_single
 
 def nvps_profit(demand:np.array, q:np.array):
     """ Profit function of the newsvendor under substitution
@@ -93,16 +161,37 @@ def nvps_profit(demand:np.array, q:np.array):
         profits = np.sum(np.matmul(q, underage) - np.matmul(np.maximum(q-demand_s, 0.), (underage+overage))) # period-wise profit (T x 1)
     return profits
 
+###################################################################### Heterogenity Metrics ###################################################################
+
 def calculate_cochran(X_test, y_test):
+    """ Calculate Cochran's Q and I² for a study with two groups
+
+    Parameters
+    ---------
+    X_test : np.array
+        Array of test data
+    y_test : np.array
+        Array of test labels
+
+    Returns
+    ---------
+    Q : float
+        Cochran's Q statistic
+    I² : float
+        I² statistic
+    p_value : float
+        p-value for Cochran's Q
+    """
     # Before calculating means and variances, check if x1 and x2 are empty
     if len(y_test) == 0 or len(X_test) == 0:
         print ("Empty")
         return np.nan, np.nan, np.nan  # Or handle this case as appropriate
     
-
+    # Initialize lists to store values for both sets
     x1 = []
     x2 = []
 
+    # Split data into two groups based on the binary indicator
     for i in range(len(X_test)):
         if X_test[i, -1] == 1:  # Check if the last column is 1
             x1.append(y_test[i])
@@ -124,49 +213,22 @@ def calculate_cochran(X_test, y_test):
     means = np.array([mean_x1, mean_x2])
     variances = np.array([variance_x1, variance_x2])
 
-    # Step 1: Calculate weights (inverse of variances)
+    # Calculate weights (inverse of variances)
     weights = 1 / variances
 
-    # Step 2: Calculate the overall weighted effect
+    # Calculate the overall weighted effect
     weighted_effect = np.sum(weights * means) / np.sum(weights)
 
-    # Step 3: Calculate Cochran's Q
+    # Calculate Cochran's Q
     Q = np.sum(weights * (means - weighted_effect) ** 2)
 
-    # Step 4: Calculate I²
+    # Calculate I²
     k = len(means)
     I2 = max(0, (Q - (k - 1)) / Q) * 100
 
-    # Step 5: Calculate p-value for Q
+    # Calculate p-value for Q
     p_value = 1 - chi2.cdf(Q, df=k-1)
 
     return Q, I2, p_value
     
-
-def get_constants(risk_factor):
-    # Parameters for multi-item newsvendor problem
-    prices = np.array([0.3, 0.5, 0.6, 0.5, 0.5, 0.5]) #price data
-    prices = prices.reshape(6,1)
-    costs = np.array([0.06, 0.06, 0.06, 0.06, 0.06, 0.06]) #cost data
-    costs = costs.reshape(6,1)
-    costs = costs * risk_factor
-    salvages = np.array([0.01, 0.01, 0.01, 0.01, 0.01, 0.01]) #salvage data
-    salvages = salvages.reshape(6,1)
-    underage_data = prices - costs 
-    overage_data = costs - salvages 
-    underage_data_single = underage_data[0,0]
-    overage_data_single = overage_data[0,0]
-
-
-
-    alpha_data = np.array([             #alpha data
-        [0.0, 0.11, 0.15, 0.14, 0.14, 0.15],
-        [0.22, 0.0, 0.08, 0.11, 0.12, 0.12],
-        [0.24, 0.07, 0.0, 0.07, 0.08, 0.07],
-        [0.26, 0.09, 0.07, 0.0, 0.12, 0.12],
-        [0.19, 0.10, 0.10, 0.12, 0.0, 0.14],
-        [0.17, 0.13, 0.11, 0.11, 0.13, 0.0]
-    ])
-    return underage_data, overage_data, alpha_data, underage_data_single, overage_data_single
-
 
